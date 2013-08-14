3fb929bddf007aca6924af8996f068abdb1aec19
Dimitar Kalinkov <dkalinkov93@gmail.com>
2013-08-14 19:01:18 +0300

    Dropbox featured

---
+++ BinaryWarfare.Client/DropboxImageManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+using Spring.Social.OAuth1;
+using Spring.Social.Dropbox.Api;
+using Spring.Social.Dropbox.Connect;
+using Spring.IO;
+
+namespace BinaryWarfare.Client
+{
+    class DropboxImageManager
+    {
+        private const string DropboxAppKey = "36onnirryzekygf";
+        private const string DropboxAppSecret = "u9spl1eitw8v49s";
+        private const string OAuthTokenFileName = "OAuthTokenFileName.txt";
+
+        public static void Start()
+        {
+            DropboxServiceProvider dropboxServiceProvider =
+                new DropboxServiceProvider(DropboxAppKey, DropboxAppSecret, AccessLevel.AppFolder);
+
+            if (!File.Exists(OAuthTokenFileName))
+            {
+                AuthorizeAppOAuth(dropboxServiceProvider);
+            }
+            OAuthToken oauthAccessToken = LoadOAuthToken();
+
+            // Login in Dropbox
+            IDropbox dropbox = dropboxServiceProvider.GetApi(oauthAccessToken.Value, oauthAccessToken.Secret);
+
+            // Create new folder
+            string unitPictures = "Unit-Pics";
+            string buildingPictures = "Building-Pics";
+
+            try
+            {
+                Entry createFolderEntry = dropbox.CreateFolderAsync(unitPictures).Result;
+            }
+            catch (Exception)
+            { }
+
+            try
+            {
+                Entry createFolderEntry = dropbox.CreateFolderAsync(buildingPictures).Result;
+            }
+            catch (Exception)
+            { }
+
+            Entry uploadFileEntry = dropbox.UploadFileAsync(new FileResource("../../pic.jpg"), "/" + unitPictures + "/pic.jpg").Result;
+
+            DropboxLink sharedUrl = dropbox.GetMediaLinkAsync(uploadFileEntry.Path).Result;
+            //Process.Start(sharedUrl.Url);   
+        }
+
+        private static OAuthToken LoadOAuthToken()
+        {
+            string[] lines = File.ReadAllLines(OAuthTokenFileName);
+            OAuthToken oauthAccessToken = new OAuthToken(lines[0], lines[1]);
+            return oauthAccessToken;
+        }
+
+        private static void AuthorizeAppOAuth(DropboxServiceProvider dropboxServiceProvider)
+        {
+            OAuthToken oauthToken = dropboxServiceProvider.OAuthOperations.FetchRequestTokenAsync(null, null).Result;
+            OAuth1Parameters parameters = new OAuth1Parameters();
+            string authenticateUrl = dropboxServiceProvider.OAuthOperations.BuildAuthorizeUrl(oauthToken.Value, parameters);
+            Process.Start(authenticateUrl);
+
+            AuthorizedRequestToken requestToken = new AuthorizedRequestToken(oauthToken, null);
+            OAuthToken oauthAccessToken = dropboxServiceProvider.OAuthOperations.ExchangeForAccessTokenAsync(requestToken, null).Result;
+
+            string[] oauthData = new string[] { oauthAccessToken.Value, oauthAccessToken.Secret };
+            File.WriteAllLines(OAuthTokenFileName, oauthData);
+        }
+    }
+}
--- BinaryWarfare.Client/Program.cs
+++ BinaryWarfare.Client/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using BinaryWarfare.Data;
+using BinaryWarfare.Data.Migrations;
+using BinaryWarfare.Model;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using BinaryWarfare.Data;
-using BinaryWarfare.Data.Migrations;
-using BinaryWarfare.Model;
 
 namespace BinaryWarfare.Client
 {
@@ -14,18 +13,20 @@ namespace BinaryWarfare.Client
     {
         static void Main()
         {
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BinaryWarfareContext, Configuration>());
-            var context = new BinaryWarfareContext();
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<BinaryWarfareContext, Configuration>());
+            //var context = new BinaryWarfareContext();
 
-            var user = new User();
-            user.Username = "Spas";
-            user.AcademyLevel = 1;
-            user.Money = 250.50M;
-            user.AuthCode = "6fa9133efe05348e430bd5a4585b595f0cb6cba3";
-            user.SessionKey = "6fa9133efe05348e430bd5a4585b595f0cb6cba3";
+            //var user = new User();
+            //user.Username = "Toncho";
+            //user.AcademyLevel = 1;
+            //user.Money = 512.50M;
+            //user.AuthCode = "6fa9133efe05348e430bd5a4585b595f0cb6cba3";
+            //user.SessionKey = "6fa9133efe05348e430bd5a4585b595f0cb6cba3";
 
-            context.Users.Add(user);
-            context.SaveChanges();
+            //context.Users.Add(user);
+            //context.SaveChanges();
+
+            DropboxImageManager.Start();            
         }
     }
 }
